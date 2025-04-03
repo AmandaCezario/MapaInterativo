80d11a203583289e760cb8ff1b9e7985a631c45f
ismailergregorio <projetointegradorextensao@gmail.com>
2025-04-03 19:59:50 -0300

    ajuste no PopUp

--- scripts/script_map.js
+++ scripts/script_map.js
@@ -17,14 +17,17 @@ L.control.zoom({
 
 
 var popupContent = `
-            <div class="popup-content">
-                <h3>Meu Local</h3>
-                <p>Este é um local muito interessante!</p>
-                <p><strong>Endereço:</strong> Rua Fictícia, 123</p>
-                <p><strong>Descrição:</strong> Localizado no coração da cidade, com acesso fácil a várias atrações turísticas.</p>
-                <p><a href="https://www.exemplo.com" target="_blank">Saiba mais</a></p>
-            </div>
-        `;
+    <div style="width: 250px; border-radius: 8px; box-shadow: 0px 4px 6px rgba(0, 0, 0, 0.1); overflow: hidden; font-family: Arial, sans-serif;">
+        <img src="img/ExemploDePopup.jpeg" alt="Image cap" style="width: 100%; height: 120px; object-fit: cover;">
+        <div style="padding: 15px;">
+            <h3 style="margin: 0 0 10px; font-size: 18px; color: #343a40;">Meu local</h3>
+            <p style="margin: 0 0 10px; font-size: 14px; color: #6c757d;">Este é um local muito interessante!.</p>
+            <p style="margin: 0 0 10px; font-size: 14px; color: #6c757d;">Endereço: Rua Fictícia, 123</p>
+            <p style="margin: 0 0 10px; font-size: 14px; color: #6c757d;">Descrição: Localizado no coração da cidade, com acesso fácil a várias atrações turísticas</p>
+            <a href="https://getbootstrap.com/docs/5.3/components/card/" target="_blank" style="display: inline-block; padding: 8px 12px; background-color: #007bff; color: white; text-decoration: none; border-radius: 4px; font-size: 14px;">Saiba mais</a>
+        </div>
+    </div>
+`;
 
 
 L.marker([-20.294915659067243, -40.347506360915474]).addTo(map)
